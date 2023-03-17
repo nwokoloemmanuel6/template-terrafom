25e046802ef39a94efc103c38095b279e328f17f
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2023-03-17 15:02:15 +0100

    Create ingress-rule.tf

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,61 @@
+resource "kubernetes_ingress_v1" "voting-ingress" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "voting.nwokolo.live"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sock-shop.nwokolo.live"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
