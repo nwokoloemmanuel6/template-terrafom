6b038abee8a16aa74a2625d83eb7c85813e13905
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2023-03-17 15:01:41 +0100

    Create provide.tf

---
+++ ingress-rule/provide.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+data "aws_eks_cluster" "demo" {
+  name = "demo"
+}
+data "aws_eks_cluster_auth" "demo_auth" {
+  name = "demo_auth"
+}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.demo.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
+  version          = "2.16.1"
+  config_path = "~/.kube/config"
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", "demo"]
+    command     = "aws"
+  }
+}
